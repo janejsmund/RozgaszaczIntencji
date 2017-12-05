d0175efa9a0ce5e3870323eb8c1c4eeee4785436
Jan Ejsmund <jan.ejsmund@gmail.com>
2017-12-05 19:56:08 +0100

    Dodano intent

--- app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
+++ app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.rozgaszaczintencji;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void broadcastIntent(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.example.admin.rozgaszaczintencji.MY_INTENT");
+        sendBroadcast(intent);
     }
 }

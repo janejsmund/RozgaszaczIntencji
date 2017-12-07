d6cd33f33570d0c3cc1fe123d1ca11c6ae54f084
janejsmund <jan.ejsmund@gmail.com>
2017-12-07 17:06:14 +0100

    Defined button and edittext

--- app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
+++ app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private EditText text;
+    private Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textToBroadcast);
+        send = findViewById(R.id.sendBroadcast);
     }
 
     public void broadcastIntent(View view) {

9b0f9d38aa7cff9a237d970bf7bcf61cb57b60e9
janejsmund <jan.ejsmund@gmail.com>
2018-01-12 11:59:04 +0100

    Działa jak powinno i loguje.

--- app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
+++ app/src/main/java/com/example/admin/rozgaszaczintencji/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.rozgaszaczintencji;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +11,7 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 
     private EditText text;
-    private Button send;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.textToBroadcast);
-        send = findViewById(R.id.sendBroadcast);
+
+        message = text.getText().toString();
     }
 
     public void broadcastIntent(View view) {
+
         Intent intent = new Intent();
-        intent.setAction("com.example.admin.rozgaszaczintencji.MY_INTENT");
+        intent.setAction("com.MY_INTENT");
+        intent.putExtra("message", message);
+
         sendBroadcast(intent);
+
+        Log.i("Custom broadcast", "Sent!");
     }
 }
